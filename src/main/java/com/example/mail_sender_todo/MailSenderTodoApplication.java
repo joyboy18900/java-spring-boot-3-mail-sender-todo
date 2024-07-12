23366abe08b62a63cec3c24d933374e2ff8c5ade
joyboy18900 <surakiat.dev@gmail.com>
2024-07-12 15:35:54 +0700

    init project java spring boot mail

---
+++ src/main/java/com/example/mail_sender_todo/MailSenderTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mail_sender_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSenderTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailSenderTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mail_sender_todo/MailSenderTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mail_sender_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
